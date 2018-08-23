13ed4edc030899c7c4326203667a16c3b9d1597a
Franco Canevali <francocanevali@gmail.com>
2018-08-22 22:11:28 -0300

    Add files via upload

--- frnkq/Clase1/Clase1_ej6/Program.cs
+++ frnkq/Clase1/Clase1_ej6/Program.cs
@@ -10,6 +10,36 @@ namespace Clase1_ej6
     {
         static void Main(string[] args)
         {
+            int year = 0;
+            do
+            {
+                year = askNumber();
+                if (checkIfBisiesto(year))
+                    Console.WriteLine("Es año bisiesto");
+            } while (year != 0);
+
+        }
+
+        public static int askNumber()
+        {
+            Console.WriteLine("Ingrese numero de año para verificar si este es bisiesto. Para salir ingrese 0 o cualquier caracter");
+            var value = Console.ReadLine();
+            int number;
+            bool success = int.TryParse(value, out number);
+            if (success)
+                return number;
+
+            return 0;
+        }
+
+        public static bool checkIfBisiesto(int year)
+        {
+            if(year % 4 == 0)
+            {
+                if ((year % 400 == 0 && year % 100 == 0 || year % 100 != 0))
+                    return true;
+            }
+            return false;
         }
     }
 }
---
+++ frnkq/Clase1/Clase1_ej7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1_ej7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 0;
+            int month = 0;
+            int year = 0;
+            char salir = 'n';
+            do
+            {
+                countLivedDays(day, month, year, DateTime.Now);
+
+
+
+                Console.WriteLine("Desea salir? s/n");
+                var exit = Console.ReadLine();
+                salir = char.Parse(exit.ToString());
+            } while (salir == n);
+        }
+
+        public static int countLivedDays(int day, int month, int year, DateTime currentDate)
+        {
+            if (day > 0 && month > 0 && year > 0)
+            {
+                if (checkIfBisiesto(year))
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+
+            return 0;
+        }
+
+        public static bool checkIfBisiesto(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if ((year % 400 == 0 && year % 100 == 0 || year % 100 != 0))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
