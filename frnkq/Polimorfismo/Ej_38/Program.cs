d6e74c71cb35367d8e25cf80ba7d26d976a67532
alumno <alumno>
2018-09-20 21:58:23 -0300

    central

---
+++ frnkq/Polimorfismo/Ej_38/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sobreescrito s = new Sobreescrito();
+            Console.WriteLine(s.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ frnkq/Polimorfismo/Ej_38/Sobreescrito.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_38
+{
+    class Sobreescrito
+    {
+        public override string ToString()
+        {
+            return "¡Este es mi método ToString sobreescrito!";
+        }
+    }
+}
